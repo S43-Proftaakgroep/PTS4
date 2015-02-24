f88b6d21521610dd77154e909b3ff49052bf07ac
Hoeks, Maikel <maikelhoeks@hotmail.com>
2015-02-24 15:04:05 +0100

    database

--- CIMS/src/java/cims/DatabaseManager.java
+++ CIMS/src/java/cims/DatabaseManager.java
@@ -5,10 +5,34 @@
  */
 package cims;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import cims.Property;
+
 /**
  *
  * @author Joris
  */
 public class DatabaseManager {
-    
+    public static Connection connection;
+
+    /**
+     * Opens the connection to the database
+     *
+     * @return Returns true if succesfull, else returns false
+     */
+    private static boolean openConnection() {
+        boolean result;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://" + Property.DBADDRESS.getProperty() + ":"+ Property.DBPORT.getProperty()+ "/"+Property.DBNAME.getProperty(), Property.DBUSERNAME.getProperty(), Property.DBPASSWORD.getProperty());
+            result = true;
+        } catch (Exception e) {
+            connection = null;
+            System.out.println(e.getMessage());
+            System.out.println("Connection failed");
+            result = false;
+        }
+        return result;
+    }
 }
