8e9ca56b247d236276f48ccc1016215ab5ead043
Meny22 <meny.metekia@gmail.com>
2015-04-14 12:28:47 +0200

    ip maikel

--- CIMS/web/js/communication.js
+++ CIMS/web/js/communication.js
@@ -68,10 +68,10 @@
                     return;
                 con.fillRect(0, 0, w, h);
                 con.drawImage(v, 0, 0, w, h);
-                var data = c.toDataURL('image/jpeg', 0.1);
+                var data = c.toDataURL('image/jpeg', 0.5);
                 newblob = convertToBinary(data);
                 ws.send(newblob);
-            }, 30);
+            }, 120);
         }, false);
 
     });
